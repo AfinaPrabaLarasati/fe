72158d3516234c94fe0bca12b9f426913d78b77c
AfinaPrabaLarasati <afinaprabalarasati@gmail.com>
2023-12-19 21:41:34 +0700

    Done

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,65 +1,75 @@
 import React, { useState } from 'react';
 import jsQR from 'jsqr';
 
-const QrCodeDetector = () => {
-  const [qrCodeValue, setQrCodeValue] = useState('');
+const QRCodeReader = () => {
+  const [qrCodeValue, setQRCodeValue] = useState('');
+  const [file, setFile] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
+  const [isLoading, setLoading] = useState(false);
 
-  const handleFileUpload = async (event) => {
+  const handleFileUpload = (event) => {
     const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    setLoading(true);
 
-    try {
-      const fileContent = await readFile(selectedFile);
-      const code = await decodeQRCode(fileContent);
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const fileContent = e.target.result;
+      setQRCodeValue('');
+      decodeQRCode(fileContent);
+      setImagePreview(fileContent);
+    };
+    reader.readAsDataURL(selectedFile);
+  };
+
+  const redirectToUrl = (url) => {
+    window.open(url, '_blank');
+  };
+
+  const decodeQRCode = async (imageData) => {
+    const image = new Image();
+    image.src = imageData;
+
+    image.onload = async () => {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      canvas.width = image.width;
+      canvas.height = image.height;
+      ctx.drawImage(image, 0, 0, image.width, image.height);
+
+      const imageDataArray = ctx.getImageData(0, 0, image.width, image.height).data;
       
-      if (code) {
-        setQrCodeValue(code.data);
-      } else {
-        setQrCodeValue('QR code not found');
-      }
-    } catch (error) {
-      console.error('Error reading or decoding QR code:', error);
-      setQrCodeValue('Error reading or decoding QR code');
-    }
-  };
+      try {
+        const code = jsQR(imageDataArray, image.width, image.height);
 
-  const readFile = (file) => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = (e) => resolve(e.target.result);
-      reader.onerror = (error) => reject(error);
-      reader.readAsDataURL(file);
-    });
-  };
-
-  const decodeQRCode = (imageData) => {
-    return new Promise((resolve, reject) => {
-      const image = new Image();
-      image.src = imageData;
-
-      image.onload = () => {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-        canvas.width = image.width;
-        canvas.height = image.height;
-        ctx.drawImage(image, 0, 0, image.width, image.height);
-
-        const imageDataArray = ctx.getImageData(0, 0, image.width, image.height).data;
-
-        try {
-          const code = jsQR(imageDataArray, image.width, image.height);
-          resolve(code);
-        } catch (error) {
-          reject(error);
+        if (code) {
+          setQRCodeValue(code.data);
+          // Redirect only if the decoded value is a valid URL
+          if (isValidUrl(code.data)) {
+            redirectToUrl(code.data);
+          }
+        } else {
+          setQRCodeValue('QR code not found');
         }
-      };
-    });
+      } catch (error) {
+        console.error('Error decoding QR code:', error);
+        setQRCodeValue('Error decoding QR code');
+      } finally {
+        setLoading(false);
+      }
+    };
+  };
+
+  const isValidUrl = (url) => {
+    // Add your own URL validation logic here
+    // You may want to check if the URL is well-formed or meets specific criteria
+    return true;
   };
 
   return (
     <div style={{ textAlign: 'center', marginTop: '20px' }}>
       <label htmlFor="fileInput" style={{ display: 'block', marginBottom: '10px' }}>
-        Upload Image:
+        Upload QR Code Image:
       </label>
       <input
         type="file"
@@ -82,6 +92,8 @@ const QrCodeDetector = () => {
         Choose File
       </button>
 
+      {isLoading && <p>Loading...</p>}
+
       <p style={{ marginTop: '20px', fontWeight: 'bold' }}>Result: {qrCodeValue}</p>
       {imagePreview && (
         <img src={imagePreview} alt="Image Preview" style={{ width: '50%', marginTop: '20px' }} />
@@ -90,4 +102,4 @@ const QrCodeDetector = () => {
   );
 };
 
-export default QrCodeDetector;
+export default QRCodeReader;
